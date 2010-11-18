d95efc7b76776d1609296cdf3b86e6fc0a68f2f5
harshada <harshada@harshada-desktop.(none)>
2010-11-18 13:22:00 +0530

    added the script to generate the debian packages.

---
+++ create_debian.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone https://harshada@github.com/zfs-linux/lzfs.git
+git clone https://harshada@github.com/zfs-linux/zfs.git
+git clone https://harshada@github.com/zfs-linux/spl.git
+# copy the ubuntu zfsload script
+cp lzfs/etc/init.d/zfsload-ubuntu lzfs/etc/init.d/zfsload
+for name in spl zfs lzfs
+do
+echo $name
+cd $name
+version=`awk '/Version/ {print $2}' META`
+cd ..
+mv $name $name-$version
+cd $name-$version
+uname=`uname -r`
+sudo debuild -i -us -uc
+cd ..
+mv $name\_0.5-1_amd64.deb $name\_$version-$uname.deb
+mv $name-$version $name
+done
