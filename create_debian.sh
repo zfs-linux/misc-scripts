0605b46fc4eec9cdbb82f4c63598c1c23b4eea1e
kqinfo <kqinfo@kqinfo-desktop.(none)>
2010-11-22 15:00:25 +0530

    Changes made to the script for the debain packaging.

--- create_debian.sh
+++ create_debian.sh
@@ -4,19 +4,42 @@ git clone git://github.com/zfs-linux/zfs.git
 git clone git://github.com/zfs-linux/lzfs.git
 git clone git://github.com/zfs-linux/spl.git
 
-# copy the ubuntu zfsload script
-cp lzfs/scripts/zfsload-ubuntu lzfs/etc/init.d/zfsload
-for name in spl zfs lzfs
+#copy the ubuntu zfsload script
+cp lzfs/etc/init.d/zfsload-ubuntu lzfs/etc/init.d/zfsload
+sudo modprobe -r zlib_deflate
+for name in zfs
 do
 echo $name
+cd spl
+splversion=`awk '/Version/ {print $2}' META`
+cd ..
 cd $name
 version=`awk '/Version/ {print $2}' META`
 cd ..
-mv $name $name-$version
-cd $name-$version
 uname=`uname -r`
+if [ "$name" = "zfs" ]
+then 
+sudo mkdir -p /usr/src/spl-$splversion/$uname/module/spl
+sudo mkdir -p /usr/src/spl-$splversion/$uname/module/splat
+sudo cp spl/module/spl/spl.ko /usr/src/spl-$splversion/$uname/module/spl
+sudo cp spl/module/splat/splat.ko /usr/src/spl-$splversion/$uname/module/splat
+fi
+mv $name $name-$version
+tar czf $name-0.5.tar.gz $name-$version
+cp $name-0.5.tar.gz $name\_0.5.orig.tar.gz
+tar -xzvf $name\_0.5.orig.tar.gz
+cd $name-$version
+
+# HERE THIS VARIABLE IS SET MANUALLY IN THE MAKEFILE.IN BUT LATER WE NEED TO SET IT TO THIS SCRIPT.
+#export env NORUNCHECK=TRUE
+#echo $NORUNCHECK
+#printenv | grep CHEC
 sudo debuild -i -us -uc
 cd ..
 mv $name\_0.5-1_amd64.deb $name\_$version-$uname.deb
+sudo dpkg -i $name\_$version-$uname.deb
 mv $name-$version $name
+sudo rm -rf /usr/src/spl-$splversion/$uname/module/spl
+sudo rm -rf /usr/src/spl-$splversion/$uname/module/splat
 done
+rm -rf *.gz *.build *.changes *.dsc
